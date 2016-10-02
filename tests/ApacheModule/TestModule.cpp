64dd1c0db00e3b7f6ebda893a240db1b749bd9fb
Karen Arutyunov <karen.arutyunov@gmail.com>
2016-10-02 19:10:09 +0400

    Copyright note changed

--- tests/ApacheModule/TestModule.cpp
+++ tests/ApacheModule/TestModule.cpp
@@ -1,20 +1,8 @@
 /*
- * Elements - useful abstractions library.
- * Copyright (C) 2005-2015 Karen Arutyunov
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * product   : Elements - useful abstractions library.
+ * copyright : Copyright (c) 2005-2016 Karen Arutyunov
+ * licenses  : GNU GPL v2; see accompanying LICENSE file
+ *             Commercial; contact karen.arutyunov@gmail.com
  */
 
 #include "CPPModule.cpp"
